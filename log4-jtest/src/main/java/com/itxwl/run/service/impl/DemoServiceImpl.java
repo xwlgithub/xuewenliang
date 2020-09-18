2696e139b4bf00047b41bebbd15e5b19a5aa2c02
xwlgithub <2509647976@qq.com>
2020-09-18 09:36:11 +0800

    Springboot整合mybatis-plus自定义数据源并结合druid连接池支持

--- log4-jtest/src/main/java/com/itxwl/run/service/impl/DemoServiceImpl.java
+++ log4-jtest/src/main/java/com/itxwl/run/service/impl/DemoServiceImpl.java
@@ -3,6 +3,7 @@ package com.itxwl.run.service.impl;
 import com.itxwl.run.service.DemoService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
