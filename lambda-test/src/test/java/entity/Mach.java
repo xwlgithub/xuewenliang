0fec0acda5c7f302b455a6cbe7e3bbfbbb05b772
xwlgithub <2509647976@qq.com>
2020-09-18 09:36:57 +0800

    Springboot整合mybatis-plus自定义数据源并结合druid连接池支持

---
+++ lambda-test/src/test/java/entity/Mach.java
@@ -0,0 +1,18 @@
+package entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @Auther: 薛
+ * @Date: 2020/6/19 10:22
+ * @Description:
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Mach {
+    private String id;
+    private String name;
+}
