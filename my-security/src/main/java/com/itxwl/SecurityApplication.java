882a0241c4f6ebab56c68d67cf977871b08c3bbf
xuewenliang <2509647976@qq.com>
2021-05-14 20:35:26 +0800

    SpringSecurity起步

---
+++ my-security/src/main/java/com/itxwl/SecurityApplication.java
@@ -0,0 +1,16 @@
+package com.itxwl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xueWenLiang
+ * @date 2021/5/14 14:36
+ * @Description 描述信息
+ */
+@SpringBootApplication
+public class SecurityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityApplication.class,args);
+    }
+}
