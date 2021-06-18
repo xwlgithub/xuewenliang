8373e212f82b658e4704611aa00605c74a087de9
xuewenliang <2509647976@qq.com>
2021-06-18 18:06:46 +0800

    测试开发分支提交

--- demo/src/main/java/com/example/annotation/MyConfigurationProperties.java
+++ demo/src/main/java/com/example/annotation/MyConfigurationProperties.java
@@ -1,16 +1,11 @@
 package com.example.annotation;
 
-import lombok.AllArgsConstructor;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * @author xueWenLiang
  * @date 2021/4/25 10:21
- * @Description 描述信息
+ * @Description 测试master分支修改得东西
  */
 @Configuration
 //@EnableConfigurationProperties(MyProperties.class)
